847c38c892d607faff0b77dc74966965d449dc63
Collins Ezeokeke <collinsmecosy@gmail.com>
2025-05-15 11:50:10 +0100

    Add BookingForm component to Home page for enhanced user interaction. Updated main layout to include booking section, improving overall page functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,21 @@ import AboutSection from "@/components/About";
 import ContactSection from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import BookingForm from '@/components/BookingForm';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main>
+      <main className="overflow-hidden">
         <Hero />
         {/* Other Sections */}
         <AboutSection />
         <ServicesSection />
+        <BookingForm />
+        {/* Booking section */}
+
+        {/* contact section */}
         <ContactSection />
       </main>
       <Footer />
